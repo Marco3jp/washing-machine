4891ab7fe8945260c45c513e5bda7f74b005c3a4
marco <28888207+Marco3jp@users.noreply.github.com>
2020-01-23 22:51:47 +0900

    Formatted

--- menu/process.c
+++ menu/process.c
@@ -1,22 +1,22 @@
-#include <manager.h>
+#include "process.h"
 
-int is_processing(){
-    if (current_process == -1){
+int is_processing() {
+    if (current_process == -1) {
         return 0;
     } else {
         return 1;
     }
 }
 
-void set_process_type(enum process_type type){
+void set_process_type(enum process_type type) {
     process = type;
 }
 
-void increment_process_type(){
-    if(process == process_type.SPIN){
+void increment_process_type() {
+    if (process == process_type.SPIN) {
         // move top
         process = process.WASH_RINS_SPIN;
-    }else {
+    } else {
         process++;
     }
 }
